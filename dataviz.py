ab8343198419088cb6304f9b5de6c87f34651637
Chang <k_chang@TSS-HR312-D024.fc.ca>
2018-11-20 14:05:43 -0500

    finished plot

--- dataviz.py
+++ dataviz.py
@@ -21,10 +21,16 @@ with open('data/googleplaystore.csv') as csvfile:
             categories.append(row)  # append = add
             line_count += 1
         else:
+            # collect the ratings info, append into ratings array
+            ratingsData = row[2]
+            ratingsData = ratingsData.replace('NaN', '0')
+            # float = number with a decimal, intergers = whole number
+            ratings.append(float(ratingsData))
+
             # print('collect the rest of data')
             # count number of rows over (from 0), push data into array
             installData = row[5]
-            installData = installData.replace(',', '')  # makes number, not text
+            installData = installData.replace(',', '')  # clean numbers
             installData = installData.replace('Free', '0')
             # installs.append(row[5]) # gives raw data
             # removes plus, get clean number you can work with
@@ -32,5 +38,33 @@ with open('data/googleplaystore.csv') as csvfile:
             line_count += 1
 
 print('processed', line_count, 'rows of data')
-print('first line:', installs[0])  # gets first row inside array
-print('last line:', installs[-1])
+print('first line:', ratings[0])  # gets first row inside array
+print('last line:', ratings[-1])
+
+np_ratings = np.array(ratings)
+
+# how many ratings are greater than 4
+popular_apps = np_ratings > 4
+pop_pct = int(len(np_ratings[popular_apps]) / len(np_ratings) * 100)
+print(pop_pct)
+
+unpopular_apps = np_ratings < 2
+not_pop_pct = int(len(np_ratings[unpopular_apps]) / len(np_ratings) * 100)
+print(not_pop_pct)
+
+mid_apps = 100 - (pop_pct + not_pop_pct)
+
+# now we can plot stuff!
+
+labels = 'Sucks, Meh, Love it!'
+sizes = [not_pop_pct, mid_apps, pop_pct]
+colors = ['yellowgreen', 'lightcoral', 'lightskyblue']
+explode = (0.1, 0.1, 0.15)
+
+plt.pie(sizes, explode=explode, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
+
+plt.legend(labels, loc=1)
+plt.title('Do We Loves Our Apps?')
+plt.xlabel('User Ratings - Google Play Store App Installs')
+plt.show()
